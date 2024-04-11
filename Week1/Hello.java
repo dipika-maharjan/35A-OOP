79d2a5fd569c39cfc660e00a67c7de4f30c15cb3
dipika-maharjan <maharjandipika49@gmail.com>
2024-04-11 10:59:42 +0545

    number one completed

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -3,7 +3,7 @@ package Tasks.Week1;
 public class Hello {
     public static void main(String[] args) {
         /*Write a program that prints Hello World! to the console. Save the program as Hello.java. */
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
     
 }
