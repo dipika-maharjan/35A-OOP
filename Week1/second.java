7caff0c8c39e4d450cb4f96af747e1886a1bfc4a
dipika-maharjan <maharjandipika49@gmail.com>
2024-04-11 10:56:37 +0545

    Number 3 completed

---
+++ Week1/second.java
@@ -0,0 +1,14 @@
+package Tasks.Week1;
+
+public class second {
+    public static void main(String[] args) {
+        
+        /* 2. Extend/Modify the above program to print two additional lines:
+        This is my first program
+        I am on module ST4003CEM
+        System.out.print (not println) and produces the same output. */
+    System.out.print("This is my first program \nI am on module ST4003CEM");
+    }
+
+
+}
---
+++ Week1/third.java
@@ -0,0 +1,16 @@
+package Tasks.Week1;
+
+public class third {
+    public static void main(String[] args) {
+        /*3. Write a program that prints the following output:
+        A "quoted" String is
+        'much' better if you learn
+        the rules of "escape sequences."
+        Also, "" represents an empty
+        String. Don't forget: use \"
+        instead of " !
+        '' is not the same as "*/
+     System.out.print("A \"quoted\" String is \n\'much\' better if you learn \nthe rules of \"escape sequences.\" \nAlso, \"\" represents an empty \nString. Don't forget: use \\\" \ninstead of \"! \n \'\' is not the same as");
+    
+    }
+    }
