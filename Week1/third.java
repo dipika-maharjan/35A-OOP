9b1a83914db464037acffd0593d6e4f4333b76a3
dipika-maharjan <maharjandipika49@gmail.com>
2024-04-11 11:02:17 +0545

    number two completed

--- Week1/third.java
+++ Week1/third.java
@@ -10,7 +10,7 @@ public class third {
         String. Don't forget: use \"
         instead of " !
         '' is not the same as "*/
-     System.out.print("A \"quoted\" String is \n\'much\' better if you learn \nthe rules of \"escape sequences.\" \nAlso, \"\" represents an empty \nString. Don't forget: use \\\" \ninstead of \"! \n \'\' is not the same as");
+     System.out.print("A \"quoted\" String is \n\'much\' better if you learn \nthe rules of \"escape sequences.\" \nAlso, \"\" representss an empty \nString. Don't forget: use \\\" \ninstead of \"! \n \'\' is not the same as");
     
     }
     }
